585908d44db07f817b6fa60db0c6256059bdb66c
tyson <tyson.g.graham>
2016-03-08 16:29:14 -0700

    Here you go Daniel. Here is the working test

--- testSteganography/image_to_image/hide_image.py
+++ testSteganography/image_to_image/hide_image.py
@@ -2,18 +2,27 @@
 import sys
 import Image, ImageOps
 
-def extract_image(from_image, s=4):
+def extract_image(from_image):
+	s=4
 	data = Image.open(from_image)
+	# print(data.size)
+	# print(data.size[0])
+	# print(data.size[1])
 	for x in range(data.size[0]):
 		for y in range(data.size[1]):
 			p = data.getpixel((x, y))
+			if(x > 200 and x < 206 and y > 200 and y < 206):
+				print((p[0] % s) * 255 / s,
+					(p[1] % s) * 255 / s,
+					(p[2] % s) * 255 / s)
 			red      = (p[0] % s) * 255 / s
 			green    = (p[1] % s) * 255 / s
 			blue     = (p[2] % s) * 255 / s
 			data.putpixel((x, y), (red, green, blue))
 	return data
 
-def hide_image(public_img, secret_img, s=4):
+def hide_image(public_img, secret_img):
+	s=4
 	#the bits we are going to overwrite
 	data = Image.open(public_img)
 	#the bits we are going to write
@@ -23,9 +32,26 @@ def hide_image(public_img, secret_img, s=4):
 			p = data.getpixel((x, y))
 			q = key.getpixel((x, y))
 			red   = p[0] - (p[0] % s) + (s * q[0] / 255)
+			if(x > 200 and x < 206 and y > 200 and y < 206):
+				print(p[0] - (p[0] % s) + (s * q[0] / 255),
+					p[1] - (p[1] % s) + (s * q[1] / 255),
+					p[2] - (p[2] % s) + (s * q[2] / 255))
+				# print('p[0], q[0]')
+				# print(p[0], q[0])
+				# print('p[1],q[1]')
+				# print(p[1],q[1])
+				# print('p[2],q[2]')
+				# print(p[2],q[2])
 			green = p[1] - (p[1] % s) + (s * q[1] / 255)
 			blue  = p[2] - (p[2] % s) + (s * q[2] / 255)
 			data.putpixel((x,y), (red,green,blue))
+			# if (red > 100 and green < 100 and blue < 100):
+				# print('x,y')
+				# print(x, y)
+				# print('Cover IMG, Hide Image')
+				# print(p,q)
+				# print('R,G,B')
+				# print(red,green,blue)
 	return data
 
 def give_help():
@@ -38,11 +64,11 @@ if (__name__=="__main__"):
 		give_help()
 
 	elif (sys.argv[1] == "show"):
-		extract_image(sys.argv[2]).save("extracted.jpg");
+		extract_image(sys.argv[2]).save("extracted.png");
 		print("image saved as extracted.png");
 
 	elif (sys.argv[1] == "hide"):
-		hide_image(sys.argv[2], sys.argv[3]).save("hidden.jpg")
+		hide_image(sys.argv[2], sys.argv[3]).save("hidden.png")
 		print "successfully hid the image"
 
 	else:
